ae3038ffd0408b951c22eb7ab154e73851dcabf6
liuluojie <qwerzxcvbnm999@163.com>
2021-08-30 15:35:36 +0800

    Initial commit

---
+++ src/com/lljieeeeee/algorithm/LeetCode.java
@@ -0,0 +1,31 @@
+package com.lljieeeeee.algorithm;
+
+import com.sun.deploy.util.ArrayUtil;
+
+/**
+ * @author Lljieeeeee
+ * @date 2021/5/27 22:49
+ * @url https://www.lljieeeeee.top/
+ * @QQ 2015743127
+ */
+public class LeetCode {
+
+    public static void main(String[] args) {
+        System.out.println(numSquares(12));
+    }
+
+    public static int numSquares(int n) {
+        int res = 0;
+        int cnt = 0;
+        for(int i = 4 ; i>0;i--) {
+            while((i*i) + cnt <= n) {
+                cnt += i*i;
+                res++;
+            }
+            if(cnt == n) {
+                break;
+            }
+        }
+        return res;
+    }
+}
